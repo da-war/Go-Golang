33e68c300aa50f9b7b7c53ce5652d9e0d14f3d0b
drkast <ranadawarabdullah>
2024-09-15 15:54:29 +0500

    functions

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	return a + b
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello, World!")
-	var x int = 5
-	var y = 10
-	fmt.Println(x)
-	fmt.Println(y)
+
+	var sumOfTwo = sum(1, 2)
+	fmt.Println(sumOfTwo)
 }
