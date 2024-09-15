117b0f96736d508ff34f341e8c1c959cb5f72923
drkast <ranadawarabdullah>
2024-09-15 16:14:51 +0500

    iterating arrays in the for loop

--- main.go
+++ main.go
@@ -3,23 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	day := 2
-	switch day {
-	case 1:
-		fmt.Println("Monday")
-	case 2:
-		fmt.Println("Tuesday")
-	case 3:
-		fmt.Println("Wednesday")
-	case 4:
-		fmt.Println("Thursday")
-	case 5:
-		fmt.Println("Friday")
-	case 6:
-		fmt.Println("Saturday")
-	case 7:
-		fmt.Println("Sunday")
-	default:
-		fmt.Println("Happy day")
+	numbers := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for index, value := range numbers {
+		fmt.Println("index", index, "value", value)
 	}
 }
