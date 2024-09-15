d7b8c378f89ad805905bb5655c95eb0e38543792
drkast <ranadawarabdullah>
2024-09-15 16:18:42 +0500

    2d arrays

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func main() {
 
-	numbers := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(numbers[i])
-	}
+	matrix := [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	fmt.Println("Matrix:", matrix)
+	fmt.Println("Matrix[1][2]:", matrix[1][2])
 }
